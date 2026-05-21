3cd787bf986a40072a24e87dd631be110c27047f
Ansxter <GAnnLiu95@gmail.com>
2026-05-21 19:39:56 +0800

    test

--- 25.py
+++ 25.py
@@ -2,10 +2,10 @@ import numpy as np
 
 data = np.genfromtxt("Grocery_Inventory_and_Sales_Dataset.csv",delimiter=",",dtype=str,skip_header=1)
 
-product_names = data[:, 1]  # 商品名稱
-stock_qty = data[:, 5].astype(int)  # 庫存數量
-unit_price = np.char.replace(data[:, 8], "$", "").astype(float)  # 單價
-sales_volume = data[:, 13].astype(int)  # 銷售量
+product_names = data[:, 1]
+stock_qty = data[:, 5].astype(int)
+unit_price = np.char.replace(data[:, 8], "$", "").astype(float)
+sales_volume = data[:, 13].astype(int)
 
 
 inventory_value = stock_qty * unit_price
